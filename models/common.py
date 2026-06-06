ac0415a64d6296d9884405778ae1cfd369cf1931
Your Name <your_email@example.com>
2026-06-06 10:36:38 +0800

    更改了c2f

--- models/common.py
+++ models/common.py
@@ -168,15 +168,20 @@ class C2fBottleneck(nn.Module):
 
 class c2f(nn.Module):
     # CSP Bottleneck with 2 convolutions
-    def __init__(self,c1,c2,n=1,shortcut=True,g=1,k=((3,3),(3,3)),e=0.5):
+    def __init__(self,c1,c2,n=1,shortcut=True,g=1,e=0.5):
         super().__init__()
         self.c = int(c2*e)
-        self.cv1 = Conv(c1,self.c,1,1)
-        self.cv2 = Conv((2+n)*self.c,self.c,1,1)
+        self.cv1 = Conv(c1,2*self.c,1,1)
+        self.cv2 = Conv((2+n)*self.c,c2,1)
         self.m = nn.ModuleList(C2fBottleneck(self.c, self.c, shortcut, g, k=((3,3),(3,3)),e = 1.0) for _ in range(n))
     def forward(self,x):
-        y = self.cv1(x)
-        return self.cv2(torch.cat([y]+[m(y) for m in self.m],1))
+        y = list(self.cv1(x).chunk(2,1))
+        y.extend(m(y[-1])for m in self.m)
+        return self.cv2(torch.cat(y,1))
+    def forward_split(self,x):
+        y = list(self.cv1(x).split((self.c,self.c),1))
+        y.extend(m(y[-1])for m in self.m)
+        return self.cv2(torch.cat(y,1))
 
 class C3(nn.Module):
     # CSP Bottleneck with 3 convolutions
