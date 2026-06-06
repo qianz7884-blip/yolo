20571eb38dedad0b2c1da62c503258cdc6218c81
Your Name <your_email@example.com>
2026-06-06 18:34:09 +0800

    增加se

--- models/common.py
+++ models/common.py
@@ -21,6 +21,7 @@ import pandas as pd
 import requests
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from IPython.display import display
 from PIL import Image
 from torch.cuda import amp
@@ -152,6 +153,21 @@ class CrossConv(nn.Module):
 
     def forward(self, x):
         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+class SE(nn.Module):
+    def __init__(self,in_chnls,ratio):
+        super(SE,self).__init__()
+        self.squeeze = nn.AdaptiveAvgPool2d((1,1))
+        self.compress = nn.Conv2d(in_chnls,in_chnls//ratio,1,1,0)
+        self.excitation = nn.Conv2d(in_chnls//ratio,in_chnls,1,1,0)
+    def forward(self,x):
+        out = self.squeeze(x)
+        out = self.compress(out)
+        out = F.relu(out)
+        out = self.excitation(out)
+        return x * F.sigmoid(out)
+
+
 
 
 class C2fBottleneck(nn.Module):
