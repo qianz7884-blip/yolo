d06e15d2424aec9ad2df52e589e854602ae744d1
Your Name <your_email@example.com>
2026-06-06 18:44:46 +0800

    使用gw调整输出维度

--- models/yolo.py
+++ models/yolo.py
@@ -340,6 +340,12 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
+        elif m is SE:
+            c1 = 512
+            c2 = args[0]
+            if c2 != no:
+                c2 = make_divisible(c2 * gw, 8)
+            args = [c1,*args[1:]]
         else:
             c2 = ch[f]
 
